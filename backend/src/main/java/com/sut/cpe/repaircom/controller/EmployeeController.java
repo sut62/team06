e882ca0b5d16094f66e90f0173493c353b889c07
XeonEVO <43061704+XeonEVO@users.noreply.github.com>
2020-01-08 00:25:32 +0700

    add EmployeeController - close #69

---
+++ backend/src/main/java/com/sut/cpe/repaircom/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.sut.cpe.repaircom.controller;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.sut.cpe.repaircom.entity.Employee;
+import com.sut.cpe.repaircom.repository.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/employee")
+    public Collection<Employee> Employees() {
+        return employeeRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
