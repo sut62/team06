6241b31432fc9685bc0734a520d64edc481c7feb
XeonEVO <43061704+XeonEVO@users.noreply.github.com>
2020-01-09 00:12:27 +0700

    edit dataloader - close #96

--- backend/src/main/java/com/sut/cpe/repaircom/RepaircomApplication.java
+++ backend/src/main/java/com/sut/cpe/repaircom/RepaircomApplication.java
@@ -1,4 +1,4 @@
-﻿package com.sut.cpe.repaircom;
+package com.sut.cpe.repaircom;
 
 import com.sut.cpe.repaircom.entity.*;
 import com.sut.cpe.repaircom.entity.Province;
@@ -20,97 +20,92 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class RepaircomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RepaircomApplication.class, args);
-		Date date = new Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-		String strDate = formatter.format(date);
-		System.out.println("Date Format with MM/dd/yyyy : "+strDate);
-		System.out.println(date);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RepaircomApplication.class, args);
+    }
 
-	@Bean
-	ApplicationRunner init(EmployeeRepository employeeRepository, ProvinceRepository provinceRepository,SexRepository sexRepository,AddproductRepository addproductRepository,TypeRepository typeRepository,BrandRepository brandRepository,PartRepository partRepository,BranchRepository branchRepository,FixtypeRepository fixtypeRepository,CustomerRepository customerrepository,PartpiecesnumberforrepairRepository partpiecesnumberforrepairRepository,StatusrepairRepository statusrepairRepository,HeadingRepository headingRepository,CommunicationRepository communicationRepository) {
-		return args -> {
-			Stream.of("Takoonkan  Bunjan", "Natalee Satong", "Sawadee Yambai").forEach(name -> {
-				Employee employee = new Employee(); // สร้าง Object Employee
-				employee.setEmpName(name); // set ชื่อ (name) ให้ Object ชื่อ Employee
-				employeeRepository.save(employee); // บันทึก Objcet ชื่อ Employee
-			});
+    @Bean
+    ApplicationRunner init(EmployeeRepository employeeRepository, ProvinceRepository provinceRepository,SexRepository sexRepository,AddproductRepository addproductRepository,TypeRepository typeRepository,BrandRepository brandRepository,PartRepository partRepository,BranchRepository branchRepository,FixtypeRepository fixtypeRepository,CustomerRepository customerrepository,PartpiecesnumberforrepairRepository partpiecesnumberforrepairRepository,StatusrepairRepository statusrepairRepository,HeadingRepository headingRepository,CommunicationRepository communicationRepository) {
+        return args -> {
+            Stream.of("Takoonkan  Bunjan", "Natalee Satong", "Sawadee Yambai").forEach(name -> {
+                Employee employee = new Employee(); // สร้าง Object Employee
+                employee.setEmpName(name); // set ชื่อ (name) ให้ Object ชื่อ Employee
+                employeeRepository.save(employee); // บันทึก Objcet ชื่อ Employee
+            });
 
-			Stream.of("ชาย", "หญิง").forEach(name -> {
-				Sex sex = new Sex(); // สร้าง Object Sex
-				sex.setSex(name); // set ชื่อ (name) ให้ Object ชื่อ Sex
-				sexRepository.save(sex); // บันทึก Objcet ชื่อ Sex
-			});
+            Stream.of("ชาย", "หญิง").forEach(name -> {
+                Sex sex = new Sex(); // สร้าง Object Sex
+                sex.setSex(name); // set ชื่อ (name) ให้ Object ชื่อ Sex
+                sexRepository.save(sex); // บันทึก Objcet ชื่อ Sex
+            });
 
-			Stream.of("นครราชสีมา", "บุรีรัมย์").forEach(name -> {
-				Province province = new Province(); // สร้าง Object Province
-				province.setProvince(name); // set ชื่อ (name) ให้ Object ชื่อ Province
-				provinceRepository.save(province); // บันทึก Objcet ชื่อ Province
-			});
-			typeRepository.save(new Type("PC"));
-			typeRepository.save(new Type("Notebook"));
+            Stream.of("นครราชสีมา", "บุรีรัมย์").forEach(name -> {
+                Province province = new Province(); // สร้าง Object Province
+                province.setProvince(name); // set ชื่อ (name) ให้ Object ชื่อ Province
+                provinceRepository.save(province); // บันทึก Objcet ชื่อ Province
+            });
+            typeRepository.save(new Type("PC"));
+            typeRepository.save(new Type("Notebook"));
 
-			brandRepository.save(new Brand("RAZER"));
-			brandRepository.save(new Brand("STEEL-SERIES"));
-			brandRepository.save(new Brand("MSI"));
-			brandRepository.save(new Brand("INTEL"));
-			brandRepository.save(new Brand("ACER"));
+            brandRepository.save(new Brand("RAZER"));
+            brandRepository.save(new Brand("STEEL-SERIES"));
+            brandRepository.save(new Brand("MSI"));
+            brandRepository.save(new Brand("INTEL"));
+            brandRepository.save(new Brand("ACER"));
 
-			partRepository.save(new Part("KEYBOARD"));
-			partRepository.save(new Part("MONITOR"));
-			partRepository.save(new Part("MOUSE"));
-			partRepository.save(new Part("CPU"));
-			partRepository.save(new Part("RAM"));
-			
-			
-			branchRepository.save(new Branch("สาขาจังหวัดกรุงเทพมหานคร"));
-			branchRepository.save(new Branch("สาขาจังหวัดปราจีนบุรี"));
-			branchRepository.save(new Branch("สาขาจังหวัดนครราชสีมา"));
-			branchRepository.save(new Branch("สาขาจังหวัดปัตตานี"));
-			branchRepository.save(new Branch("สาขาจังหวัดยะลา"));
-			fixtypeRepository.save(new Fixtype("Computer"));
-			fixtypeRepository.save(new Fixtype("Notebook"));
+            partRepository.save(new Part("KEYBOARD"));
+            partRepository.save(new Part("MONITOR"));
+            partRepository.save(new Part("MOUSE"));
+            partRepository.save(new Part("CPU"));
+            partRepository.save(new Part("RAM"));
 
 
-			branchRepository.save(new Branch("ห้างโคราช"));
-			branchRepository.save(new Branch("มทส"));
-			brandRepository.save(new Brand("acer"));
-			brandRepository.save(new Brand("msi"));
+            branchRepository.save(new Branch("สาขาจังหวัดกรุงเทพมหานคร"));
+            branchRepository.save(new Branch("สาขาจังหวัดปราจีนบุรี"));
+            branchRepository.save(new Branch("สาขาจังหวัดนครราชสีมา"));
+            branchRepository.save(new Branch("สาขาจังหวัดปัตตานี"));
+            branchRepository.save(new Branch("สาขาจังหวัดยะลา"));
+            fixtypeRepository.save(new Fixtype("Computer"));
+            fixtypeRepository.save(new Fixtype("Notebook"));
 
 
-			statusrepairRepository.save(new Statusrepair("รับเรื่องแล้ว"));
-			statusrepairRepository.save(new Statusrepair("กำลังดำเนินการ"));
-			statusrepairRepository.save(new Statusrepair("รออะไหล่"));
-			statusrepairRepository.save(new Statusrepair("ดำเนินการเสร็จสิ้น"));
-			partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(1));
-			partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(2));
-			partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(3));
-			partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(4));
-			partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(5));
+            branchRepository.save(new Branch("ห้างโคราช"));
+            branchRepository.save(new Branch("มทส"));
+            brandRepository.save(new Brand("acer"));
+            brandRepository.save(new Brand("msi"));
 
-          			communicationRepository.save(new Communication("ติดต่อกลับทาง อีเมล"));//ระบบติดต่อสอบถาม
-			communicationRepository.save(new Communication("ติดต่อกลับทาง โทรศัพท์"));//ระบบติดต่อสอบถาม
-			headingRepository.save(new Heading("สอบถามข้อมูลเกี่ยวการแจ้งซ่อม"));//ระบบติดต่อสอบถาม
-			headingRepository.save(new Heading("สอบถามข้อมูลอาการและปัญหาคอมพิวเตอร์"));//ระบบติดต่อสอบถาม	
-			headingRepository.save(new Heading("แจ้งเรื่องร้องเรียน"));//ระบบติดต่อสอบถาม
-			headingRepository.save(new Heading("สอบถามเรื่องอื่นๆ"));//ระบบติดต่อสอบถาม
+
+            statusrepairRepository.save(new Statusrepair("รับเรื่องแล้ว"));
+            statusrepairRepository.save(new Statusrepair("กำลังดำเนินการ"));
+            statusrepairRepository.save(new Statusrepair("รออะไหล่"));
+            statusrepairRepository.save(new Statusrepair("ดำเนินการเสร็จสิ้น"));
+            partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(1));
+            partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(2));
+            partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(3));
+            partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(4));
+            partpiecesnumberforrepairRepository.save(new Partpiecesnumberforrepair(5));
+
+            communicationRepository.save(new Communication("ติดต่อกลับทาง อีเมล"));//ระบบติดต่อสอบถาม
+            communicationRepository.save(new Communication("ติดต่อกลับทาง โทรศัพท์"));//ระบบติดต่อสอบถาม
+            headingRepository.save(new Heading("สอบถามข้อมูลเกี่ยวการแจ้งซ่อม"));//ระบบติดต่อสอบถาม
+            headingRepository.save(new Heading("สอบถามข้อมูลอาการและปัญหาคอมพิวเตอร์"));//ระบบติดต่อสอบถาม
+            headingRepository.save(new Heading("แจ้งเรื่องร้องเรียน"));//ระบบติดต่อสอบถาม
+            headingRepository.save(new Heading("สอบถามเรื่องอื่นๆ"));//ระบบติดต่อสอบถาม
 
 
 
-			
-			addproductRepository.findAll().forEach(System.out::println);
-			typeRepository.findAll().forEach(System.out::println);
-			brandRepository.findAll().forEach(System.out::println);
-			partRepository.findAll().forEach(System.out::println);
-                                                      branchRepository.findAll().forEach(System.out::println);
-			provinceRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Province บน Terminal
-			employeeRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Employee บน Terminal
-			sexRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Sex บน Termina
-                                                      communicationRepository.findAll().forEach(System.out::println);//ระบบติดต่อสอบถาม
-			headingRepository.findAll().forEach(System.out::println);//ระบบติดต่อสอบถาม
 
-		};
-	}
+            addproductRepository.findAll().forEach(System.out::println);
+            typeRepository.findAll().forEach(System.out::println);
+            brandRepository.findAll().forEach(System.out::println);
+            partRepository.findAll().forEach(System.out::println);
+            branchRepository.findAll().forEach(System.out::println);
+            provinceRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Province บน Terminal
+            employeeRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Employee บน Terminal
+            sexRepository.findAll().forEach(System.out::println); // แสดง ข้อมูลทั้งหมดใน Entity Sex บน Termina
+            communicationRepository.findAll().forEach(System.out::println);//ระบบติดต่อสอบถาม
+            headingRepository.findAll().forEach(System.out::println);//ระบบติดต่อสอบถาม
+
+        };
+    }
 }
