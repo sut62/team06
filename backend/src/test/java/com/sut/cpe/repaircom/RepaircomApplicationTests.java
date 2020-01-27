d5e14fe05d7242f33a7ccb556e8963b0cfe98926
thanaphon puputta <nattapang.p@gmail.com>
2020-01-28 01:12:04 +0700

    close #232 - ทำการแก้ไข build.gradlew เพื่ออัพทเดทเวอร์ชั่นให้แสดงผล testpassได้

--- backend/src/test/java/com/sut/cpe/repaircom/RepaircomApplicationTests.java
+++ backend/src/test/java/com/sut/cpe/repaircom/RepaircomApplicationTests.java
@@ -1,7 +1,6 @@
 package com.sut.cpe.repaircom;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
